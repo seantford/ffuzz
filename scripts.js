c9f605f482549980787db0ab0708af9daa495657
Sean Ford <seanthomasford@gmail.com>
2015-06-09 23:19:27 -0400

    code for ffuzzdotcom: html, css, js

---
+++ scripts.js
@@ -0,0 +1,38 @@
+//what do i want
+	//emailer
+//how do i make it
+	//lightbox emailer that pops up on arrival
+	
+//create a pop up form
+//add input for name
+//add input for email 
+//add input for email confirmation
+//comment section
+//add checkboxes for interests
+//add submit button
+//add closeout button and 'esc' to close functionality
+
+// define your variables
+var usernameInput = getElementsById("name")
+var username = ;
+
+var emailInput = getElementsById("email")
+var useremail = emailInput.toLowerCase()
+
+var emailConfirmInput = getElementsById("emailconfirm")
+
+
+var submitButton = getElementsById("signup-submit")
+var cancelButton = getElementsById("signup-cancel")
+
+var closeButton = getElementsById("close-button")
+
+var overlay = getElementsById("black-overlay")
+
+var interestsList = {}
+
+//once
+var confirmEmail = function() {
+	useremail = 
+}
+
